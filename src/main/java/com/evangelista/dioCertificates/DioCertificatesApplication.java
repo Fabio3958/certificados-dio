06fa0e3ab3e029fab4c003731a6bc3c22fd71008
fabio <fabioevangelista_@hotmail.com>
2022-11-26 17:42:00 -0300

    first commit - model criado

---
+++ src/main/java/com/evangelista/dioCertificates/DioCertificatesApplication.java
@@ -0,0 +1,13 @@
+package com.evangelista.dioCertificates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCertificatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCertificatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evangelista/dioCertificates/DioCertificatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evangelista.dioCertificates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCertificatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
